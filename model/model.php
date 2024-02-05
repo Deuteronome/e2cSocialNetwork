6d4e1ce0dbe0a81b1ba622dbd5fde4363ed00e49
Deuteronome <o.burcker@e2c-grandlille.fr>
2024-02-05 12:01:34 +0100

    Structure de base

---
+++ model/model.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Fonction de connexion à la base de données
+ * 
+ * pas de paramètre
+ * 
+ * retourne un objet PDO
+ */
+function connectDb() {
+    
+}
+
+/*Ensuite les fonction d'accès aux données (CRUD : create, read, update, delete)*/
