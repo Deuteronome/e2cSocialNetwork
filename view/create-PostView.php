8b7b7397565b5f2ae8e7f38e9bd5c99b539ad9ee
NonodeE2C <drtnono90@gmail.com>
2024-02-16 09:35:21 +0100

    creation post et profil

---
+++ view/create-PostView.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><!-- Titre de la page - en règle général une variable venant du controller --></title>
+    <link rel="stylesheet" href="../style/bootstrap.css">
+    <link rel="stylesheet" href="../style/style.css">
+    <script src="../script/bootstrap.bundle.js"></script>
+</head>
+<body class="lineaire-simple">
+
+
+<main class="container-fluid">
+    <div class="row justify-content-between mt-4">
+
+        <div class="col-size mx-4 p-1 text-center">
+            <img src="../asset/pictogrames/fleche-pictogram.png" class="img-fluid" alt="retour">
+        </div>
+
+        <div class="col-size mx-4 p-1 text-center">
+            <img src="../asset/pictogrames/Iconakar-pencil.png" class="img-fluid" alt="retour">
+        </div>
+    </div>
+
+    <div class="row justify-content-between align-items-end mt-4">
+        <div class="pdp-profil ms-4 p-0"> 
+            
+        </div>
+        <h2 class="col-7 m-0 p-0 text-center">Thomas Chartier</h2>
+    </div>
+
+    <div class="row justify-content-between align-items-end mt-4">
+        <div class="col-3 espace-emoji ms-4"></div>
+        <div class="col-7 barre-emoji me-4">
+            <div class="row p-1">
+                <div class="col-2 p-0"><img src="../asset/emoji/emoji-blaze.png" alt="" class="img-fluid"></div>
+                <div id="langue" class="col-2 p-0"><img src="../asset/emoji/emoji-langue.png" alt="" class="img-fluid"></div>
+                <div class="col-2 p-0"><img src="../asset/emoji/emoji-love.png" alt="" class="img-fluid"></div>
+                <div class="col-2 p-0"><img src="../asset/emoji/emoji-malade.png" alt="" class="img-fluid"></div>
+                <div class="col-2 p-0"> <img src="../asset/emoji/emoji-reflechi.png" alt="" class="img-fluid"></div>
+                <div class="col-2 p-0"><img src="../asset/emoji/emoji-triste.png" alt="" class="img-fluid"></div>
+
+                <div class="row mt-4 mx-3">
+         <h2>Bio:</h2>
+        <div class="col-12 caree-connexion p-3">
+        <div >
+            <h3 class="">Zone de texte!</h3> 
+
+        </div>
+
+
+        </div>
+    </div> 
+    
+    <div class="row justify-content-start mt-4 mx-1 p-3">
+        <a class="text-connect" href="https:\\linkedin.com">
+            <div class="col-12 barre-emoji ">
+                <div class="row justify-content-between align-items-center">
+                    <div class="col-3">
+                        <img src="../asset/src/logo-linkedin.png" alt=""class="img-fluid logo-adjust" class="justify-content-start">
+                    </div>
+                    <h1 class="col-6">Connecter</h1>
+                </div>
+                
+            </div>
+        </a>
+        
+    </div>
+
+    <div class="row justify-content-start mt-4 mx-1 p-3">
+        <a class="text-connect" href="https://github.com/">
+            <div class="col-12 barre-emoji ">
+                <div class="row justify-content-between align-items-center">
+                    <div class="col-3">
+                        <img src="../asset/src/logo-gitub.png" alt=""class="img-fluid logo-adjust" class="justify-content-start">
+                    </div>
+                    <h1 class="col-6">Connecter</h1>
+                </div>
+                
+            </div>
+        </a>
+        
+    </div>
+
+    
+    
+        
+
+</main>
+</body>
+</html>
