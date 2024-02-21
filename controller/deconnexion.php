a321c1388373e72f84c06d6b93385afe56a5f145
Bigpoint59 <valentinayaka@outlook.fr>
2024-02-21 11:15:38 +0100

    Deconnexion

--- controller/deconnexion.php
+++ controller/deconnexion.php
@@ -1,14 +1,5 @@
 <?php
 session_start();
 session_destroy();
-
-
-
-require_once('../model/model.php');
-
-
-
-
-require_once('../controller/connexion.php');
-
+header('Location:connexion.php');
 ?>
